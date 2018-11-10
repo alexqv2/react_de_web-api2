84a526176fa85b8a09f26955372a5ab8434d2206
alexqv2 <alex-92quintana@hotmail.com>
2018-11-10 11:29:01 -0500

    separacion
    asd

---
+++ src/routes/HomePage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+import Dashboard from '../../components/Dashboard';
+
+const HomePage = () => (
+    <Layout>
+        <Dashboard/>
+    </Layout>
+);
+
+export default HomePage;
---
+++ src/routes/TasksPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Tasks from "../../components/Tasks";
+import Layout from '../../components/Layout';
+
+const TasksPage = () => (
+    <Layout>
+        <Tasks/>
+    </Layout>
+);
+
+export default TasksPage;
